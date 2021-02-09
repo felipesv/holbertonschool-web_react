048df3824a0549ab4f8c0c171622bac19eef393f
Felipe Satizabal <1165@holbertonschool.com>
2021-02-09 09:32:20 -0500

    [add] task 0 - 10

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,51 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1 = { firstName: 'Bruce', lastName: 'Patrick', age: 33, location: 'New York' };
+const student2 = { firstName: 'Alexa', lastName: 'Robertson', age: 45, location: 'LA' };
+const studentsList: Array<Student> = [student1, student2];
+
+/* Vanilla JS */
+const table = document.createElement('table');
+document.body.appendChild(table);
+
+const thead = document.createElement('thead');
+table.appendChild(thead);
+
+const tbody = document.createElement('tbody');
+table.appendChild(tbody)
+
+const tr = document.createElement('tr');
+thead.appendChild(tr);
+
+const thFields = ['firstName', 'lastName', 'age', 'location'];
+thFields.forEach(element => {
+  const th = document.createElement('th');
+  th.innerHTML = element;
+  tr.appendChild(th);
+});
+
+studentsList.forEach(element => {
+  const trTb = document.createElement('tr');
+  tbody.appendChild(trTb);
+
+  let td = document.createElement('td');
+  td.innerHTML = element.firstName;
+  trTb.appendChild(td);
+
+  td = document.createElement('td');
+  td.innerHTML = element.lastName;
+  trTb.appendChild(td);
+
+  td = document.createElement('td');
+  td.innerHTML = element.age.toString();
+  trTb.appendChild(td);
+
+  td = document.createElement('td');
+  td.innerHTML = element.location;
+  trTb.appendChild(td);
+});
---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* Task 1 */
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+/* Task 2 */
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+
+/* Task 3 */
+interface PrintTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+
+function printTeacher(TeacherFn: PrintTeacherFunction): string {
+  return `${TeacherFn.firstName.charAt(0)}. ${TeacherFn.lastName}`;
+}
+
+/* Task 4 */
+interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework: () => string;
+  displayName: () => string;
+}
+
+interface StudentConstructor{
+  new(firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements Student{
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+/* TESTS
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+const funct: PrintTeacherFunction = {
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+console.log(printTeacher(funct));*/
