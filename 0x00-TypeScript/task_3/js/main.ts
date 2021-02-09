9b656586ae968cbd83211d64e899f5759831b0c0
Felipe Satizabal <1165@holbertonschool.com>
2021-02-09 16:25:39 -0500

    [fix] task 8

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
-
 /// <reference path="./crud.d.ts" />
 import { RowID, RowElement } from "./interface";
 import * as CRUD from './crud';
