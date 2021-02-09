a513f3ad60bbf3a87995c5612749785340f27fbf
Felipe Satizabal <1165@holbertonschool.com>
2021-02-09 12:46:32 -0500

    [fix] task 7

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -64,7 +64,7 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
 /* Task 7 */
 type Subjects = 'Math' | 'History';
 
-export function teachClass(todayClass: Subjects): string {
+export function teachClass(todayClass:Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
   } else if (todayClass === 'History') {
