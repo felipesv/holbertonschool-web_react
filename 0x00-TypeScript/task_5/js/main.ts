a5b1110774b31467e6851960720a053d0a0dd819
Felipe Satizabal <1165@holbertonschool.com>
2021-02-09 15:33:22 -0500

    [fix] task 11

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,21 +1,25 @@
 export interface MajorCredits {
   credits: number;
+  brand?: string;
 }
 
 export interface MinorCredits {
   credits: number;
+  brand?: string;
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
   const major: MajorCredits = {
-    credits: subject1.credits + subject2.credits
+    credits: subject1.credits + subject2.credits,
+    brand: 'Major',
   }
   return major;
 };
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   const minor: MinorCredits = {
-    credits: subject1.credits + subject2.credits
+    credits: subject1.credits + subject2.credits,
+    brand: 'Minor',
   }
   return minor;
 };
