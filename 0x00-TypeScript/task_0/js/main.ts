17648c842b145be420e9006f1b502b6ab1f2eeaf
Felipe Satizabal <1165@holbertonschool.com>
2021-02-09 10:00:26 -0500

    [fix] students

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -5,42 +5,38 @@ interface Student {
   location: string;
 }
 
-const student1 = { firstName: 'Bruce', lastName: 'Patrick', age: 33, location: 'New York' };
-const student2 = { firstName: 'Alexa', lastName: 'Robertson', age: 45, location: 'LA' };
+const student1: Student = { firstName: 'Bruce', lastName: 'Patrick', age: 33, location: 'New York' };
+const student2: Student = { firstName: 'Alexa', lastName: 'Robertson', age: 45, location: 'LA' };
 const studentsList: Array<Student> = [student1, student2];
 
 /* Vanilla JS */
 const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-
 const table: HTMLTableElement = document.createElement('table');
-body.appendChild(table);
-
 const thead: HTMLTableSectionElement = document.createElement('thead');
-table.appendChild(thead);
-
 const tbody: HTMLTableSectionElement = document.createElement('tbody');
-table.appendChild(tbody)
-
 const tr: HTMLTableRowElement = document.createElement('tr');
-thead.appendChild(tr);
+const th1: HTMLTableCellElement = document.createElement('td');
+const th2: HTMLTableCellElement = document.createElement('td');
 
-const th1: HTMLTableCellElement = document.createElement('th');
 th1.innerHTML = 'firstName';
-tr.appendChild(th1);
-
-const th2: HTMLTableCellElement = document.createElement('th');
 th2.innerHTML = 'location';
-tr.appendChild(th2);
+
+body.append(table);
+table.append(thead);
+table.append(tbody)
+thead.append(tr);
+tr.append(th1);
+tr.append(th2);
 
 studentsList.forEach(element => {
   const trTb: HTMLTableRowElement = document.createElement('tr');
-  tbody.appendChild(trTb);
+  tbody.append(trTb);
 
   let td: HTMLTableCellElement = document.createElement('td');
   td.innerHTML = element.firstName;
-  trTb.appendChild(td);
+  trTb.append(td);
 
   td = document.createElement('td');
   td.innerHTML = element.location;
-  trTb.appendChild(td);
+  trTb.append(td);
 });
