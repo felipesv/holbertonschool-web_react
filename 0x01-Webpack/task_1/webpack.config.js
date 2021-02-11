f49073727d758eb9164f30aff04b38fbd45e4fd0
Felipe Satizabal <1165@holbertonschool.com>
2021-02-11 12:21:13 -0500

    [fix] change entry

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -2,9 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: 'production',
-  entry: {
-    app: './js/dashboard_main.js',
-  },
+  entry: './js/dashboard_main.js',
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
