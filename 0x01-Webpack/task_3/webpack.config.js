5192493c4f8e2f334fb1378927edb0808744adf5
Felipe Satizabal <1165@holbertonschool.com>
2021-02-11 17:39:41 -0500

    [fix] .json and webpack config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -3,52 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js'
-  },
-  performance: {
-    maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public')
+    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, './public'),
-    compress: true,
-    port: 8564,
+    contentBase: "./public",
+    port: 8564
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
   ],
+  mode: "development",
+  output: {
+    path:path.resolve(__dirname, "public"),
+    filename: "[name].bundle.js"
+  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
         ]
-      }, 
+      },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
-      },
+        ]
+      }
     ]
   }
-};
+}
