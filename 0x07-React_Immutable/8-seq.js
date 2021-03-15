6954fbd402ef9e658138d4045675eb8d42f5b553
Felipe Satizabal <1165@holbertonschool.com>
2021-03-15 16:08:06 -0500

    [add] task 8

---
+++ 0x07-React_Immutable/8-seq.js
@@ -0,0 +1,13 @@
+import immutable from 'immutable';
+
+export default function printBestStudents(object) {
+  const data = immutable.Seq(object).filter((item) => item.score > 70).map((item) => {
+    return {
+      score: item.score,
+      firstName: item.firstName.charAt(0).toUpperCase() + item.firstName.slice(1),
+      lastName: item.lastName.charAt(0).toUpperCase() + item.lastName.slice(1),
+    }
+  });
+
+  console.log(data.toJS());
+}
