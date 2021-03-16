42c6e0d432c43395897357f7997f5c9586142071
Felipe Satizabal <1165@holbertonschool.com>
2021-03-16 13:36:05 -0500

    [fix] requirement

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,17 +1,18 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(object) {
-  const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  }
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 
-  console.log(
-    Seq(object)
+export default function printBestStudents(object) {
+  const data = Seq(object)
     .filter((item) => item.score > 70).map((item) => {
       return {
         ...item,
         firstName: capitalize(item.firstName),
         lastName: capitalize(item.lastName),
       }
-  }).toJS());
+  });
+
+  console.log(data.toJS());
 }
